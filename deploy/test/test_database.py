b4f361647cbf1c6529bc953450cd29b0b0197f81
Sadegh Khosravi <khosravi74sadegh@gmail.com>
2022-09-20 11:22:41 +0430

    commit database file, edit assertion in test database

--- deploy/test/test_database.py
+++ deploy/test/test_database.py
@@ -12,7 +12,7 @@ class TestDataBase(unittest.TestCase):
 
     def test_insertObject(self):
         result = self.db.insertInObjectTable(category="test_cat", type="test_type", color="test_color")
-        self.assertNotEqual(result, -1, "Insert in Objects Table:Error happened")
+        self.assertGreaterEqual(result, -1, "Insert in Objects Table:Error happened")
 
     def test_insertDetectedObject(self):
         result = self.db.insertInDetectedObject(object_id=1, model_id=1, accuracy=96.3, timestamp=123456)
